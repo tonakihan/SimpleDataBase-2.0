adf951cba494acd6cfd9878a03b8c0601668deb1
tonakuhi <markgetman2004@gmail.com>
2023-05-18 22:01:52 +0900

    Restructuring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
 fn main() {
-    println!("Hello, world!");
+    use std::process::exit;
+    use std::env;
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Not found arguments");
+        exit(1);
+    }
+
+    //Думаю над индексами
+    if let Some(index) = args.iter().position(|arg| arg == "-m") {
+        if index + 1 < args.len() {
+            println!("Text: {}", args[index + 1]);
+        } else {
+            println!("No text provided after -m");
+        }
+    }
 }
