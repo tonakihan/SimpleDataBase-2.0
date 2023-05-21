ee24282f17b4331e1ebe7f77a44ca94ea338a7f3
tonakuhi <markgetman2004@gmail.com>
2023-05-21 21:15:46 +0900

    Last works result

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![allow(non_snake_case)]
 
+use rusqlite::{Connection, Result};
+
+
 fn main() {
     use std::process::exit;
     use std::env;
@@ -15,36 +18,52 @@ fn main() {
 }
 
 
-fn set_mode(args: &Vec<String>) -> &str {
-    //if let Some(index) = args.iter().position(|val| val == key){
-    match(args[index].to_str()) {
-        "-I" => return "-I",
-        "-S" => return "-S",
-        "-U" => println!("В разработке"),
+fn set_mode(args: &Vec<String>) -> Result<()>{//, &'static str> {
+    match args[1].as_str() {
+        "-I" => DataForDB::insert_db(&args)?,
+        "-S" => DataForDB::select_db(&args)?,
         _ => {
             println!("Not found  arguments");
-            return "";
+            //return Err("Not found arguments");
         },
     }
+    Ok(())
 }
 
-fn get_data(args: &Vec<String>) {
-    struct ins {
-        table: String,
-        column: Vec<String>,
-        value: Vec<String>,
-    }
 
-    let data: ins;
-    for index in 3..args.len() {
-        match(args[index].to_str()) {
-            "-t" => data.table = args[index+1],
-            "-c" => , //Нужно сделать эту функцию рекурсивной и вылудить данные параметров
-            "-v" => ,
-            _ => ,
-        } 
-    }
+struct DataForDB {
+    target: String,
+    column: Vec<String>,
+    value: Vec<String>,
 }
 
-fn insert_db(args: &Vec<String>);
-fn select_db(args: &Vec<String>);
+impl DataForDB {
+    fn get_data(args: &Vec<String>) -> DataForDB {
+        let obj_data: DataForDB;
+        for index in 3..args.len() {
+            match args[index].as_str() {
+                "-t" => obj_data.target = args[index+1],
+                "-c" => obj_data.column = get_value_args(args[index+1..]),
+                "-v" => obj_data.value = get_value_args(args[index+1..]),
+                _ => println!("Error: что-то пошло не так. {}", args[index]),
+            } 
+        }
+        return obj_data;
+
+        fn get_value_args(args: Vec<String>) {
+            let result = String::from("");
+            for element in args {
+                if element.get(0) != Some("-") { result += &element }
+                else { break }
+            }
+            return result;
+        }
+    }
+
+    pub fn insert_db(args: &Vec<String>) -> Result<()>{
+        Self::get_data(&args);
+    }
+    pub fn select_db(args: &Vec<String>) -> Result<()>{
+        Self::get_data(&args);
+    }
+}
