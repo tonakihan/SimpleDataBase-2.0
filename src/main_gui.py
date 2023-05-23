a8a65ecd88dc508f890dc98e5593eb74116d74cd
porovos <shevchenkoiluha@yandex.ru>
2023-05-23 21:05:31 +0900

    Better window

--- src/main_gui.py
+++ src/main_gui.py
@@ -1,9 +1,33 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import (QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout)
+from PyQt6.QtCore import Qt
+import sys
 
-app = QApplication([])
-
-window = QWidget()
-
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(300, 250)
+        self.setWindowTitle("I like cats")
+ 
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+ 
+        self.input = QLineEdit()
+        self.input.setFixedWidth(150)
+        layout.addWidget(self.input, alignment= Qt.AlignmentFlag.AlignCenter)
+ 
+        button = QPushButton("Get Text")
+        button.clicked.connect(self.get)
+        layout.addWidget(button)
+ 
+        button = QPushButton("Clear Text")
+        button.clicked.connect(self.input.clear)
+        layout.addWidget(button)
+ 
+    def get(self):
+        text = self.input.text()
+        print(text)
+ 
+app = QApplication(sys.argv)
+window = Window()
 window.show()
-
-app.exec()
+sys.exit(app.exec())
